7af0ebe2563733f6e069f853b14aa4b9da3ab85a
Paul Razvan Berg <hello@paulrberg.com>
2021-01-10 18:05:57 +0200

    feat: roll v1.0.2

--- Formula/multisol.rb
+++ Formula/multisol.rb
@@ -4,12 +4,12 @@
 class Multisol < Formula
     desc "CLI application for verifying Solidity contracts on Etherscan"
     homepage "https://github.com/paulrberg/multisol"
-    url "https://github.com/paulrberg/multisol/releases/latest/download/multisol-1.0.1-x86_64-apple-darwin.tar.gz"
-    sha256 "335d97134db54a86848dd5aff0b301de44bb44ce69491b0a027b88e4dc839aa3"
-    version "1.0.1"
+    url "https://github.com/paulrberg/multisol/releases/latest/download/multisol-1.0.2-x86_64-apple-darwin.tar.gz"
+    sha256 "41450118eb6b28ffeee4c6067f4f5077d53940962bdce192e50185b3bdf5f4b3"
+    version "1.0.2"
 
     def install
-      bin.install "multisol-1.0.1-x86_64-apple-darwin" => "multisol"
+      bin.install "multisol-1.0.2-x86_64-apple-darwin" => "multisol"
 
     end
   end
